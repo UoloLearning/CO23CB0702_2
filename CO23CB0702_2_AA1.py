d64b17d48e46b03e1a532b313014a3745d2c605f
UoloLearning <127477826+UoloLearning@users.noreply.github.com>
2023-04-17 16:52:06 +0530

    Update CO23CB0702_2_AA1.py

--- CO23CB0702_2_AA1.py
+++ CO23CB0702_2_AA1.py
@@ -1,9 +1,2 @@
-num = input("Enter a number to check if it's divisible by 5: ")
-num = int(num)
-remainder = num % 5
-while remainder != 0:
-    print(num, "is not divisible by 5.")
-    num = input("Enter a new number to check: ")
-    num = int(num)
-    remainder = num % 5
-print(num, "is divisible by 5.")
+for i in range(1, 11, 2):
+    print(i)
